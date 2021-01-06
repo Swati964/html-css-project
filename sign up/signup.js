f132781eeb38567b04a60bb3c96f3f3b09942a15
Swati964 <swatiahirwar8696@gmail.com>
2021-01-06 10:02:33 +0530

    change

--- sign up/signup.js
+++ sign up/signup.js
@@ -1,6 +1,7 @@
 function validate() {
     var username=document.getElementById("username").value;
     var password=document.getElementById("password").value;
+    console.log("you're username is" + username + "and your password is " + password)
     if(username=="admin"&& password=="user")
     {
         alert("login succesful");
